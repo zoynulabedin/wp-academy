9e008212bd3264b3abbbe124f46de9b525aeb197
Md zoynul abedin <trustcodernet@gmail.com>
2021-09-10 14:33:58 +0600

    plugin initiionzaion

---
+++ wp-academy.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+Plugin Name: Wp Academy
+Description: learning purpse
+Plugin URI: www.zoynul.com
+Author: zoynul
+Author URI:zoynul.com
+Version:1.0
+License:GPL2 or later
+Text Domain: wp-academy
+Domain Path: /lang
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+
+
+/**
+ * The main
+ */
+
+ class ClassName extends AnotherClass {
+     function __construct(argument) {
+         # code....
+     }
+ }

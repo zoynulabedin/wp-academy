8cf91b2e9241efbcbc3e59f7ca788480ad5a22f3
Md zoynul abedin <trustcodernet@gmail.com>
2021-09-11 00:41:56 +0600

    update

--- wp-academy.php
+++ wp-academy.php
@@ -18,16 +18,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 
 /**
- * The main
+ * The main class
  */
 
-Final class WPAcademy {
-    private function __construct() {
-       
+
+
+final class WpAcademy {
+    
+    /**
+     *  plugin version
+     */
+  
+     const version = '1.0';
+
+
+    /**
+     *  class constructor
+     */
+    
+   private  function __construct() {
+        
+
+
      }
 
+
      public static function init(){
-         static $instance =  false;
+         static $instance = false;
 
          if(!$instance){
              $instance = new self();
@@ -35,12 +52,24 @@ Final class WPAcademy {
          return $instance;
      }
 
+     public function define_constant(){
+         define('WP_ACADEMY_VERSION',self::version);
+         define("WP_ACADEMY_FILE",__FILE__);
+         define("WP_ACADEMY_PATH",__DIR__);
+         define("WP_ACADEMY_URL",plugins_url('', WP_ACADEMY_FILE ));
+         define("WP_ACADEMY_ASSETS",WP_ACADEMY_URL .'/assets');
+     }
+
+ }
+
+
 /**
- * plugin initilizes
- * @return \WpAcademy
+ * initialize the main plugin
+ * @return  \WpAcademy
  */
 
-     function wp_academy(){
-        WPAcademy::init();
-     }
+ function wp_academy(){
+     return WpAcademy::init();
  }
+ // kick of the plugin
+ wp_academy();
